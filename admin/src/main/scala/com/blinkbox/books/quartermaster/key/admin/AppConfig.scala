44cc91f60ba62a5bdd6a364375f805a9d792fecb
JP Hastings-Spital <jphastings@blinkbox.com>
2015-01-20 10:25:11 +0000

    Remove DbSettings code, use common config

--- admin/src/main/scala/com/blinkbox/books/quartermaster/key/admin/AppConfig.scala
+++ admin/src/main/scala/com/blinkbox/books/quartermaster/key/admin/AppConfig.scala
@@ -3,11 +3,11 @@ package com.blinkbox.books.quartermaster.key.admin
 import com.blinkbox.books.config._
 import com.typesafe.config.Config
 
-case class AppConfig(api: ApiConfig, db: DbSettings)
+case class AppConfig(api: ApiConfig, db: DatabaseConfig)
 
 object AppConfig {
   def apply(config: Config): AppConfig = AppConfig(
     ApiConfig(config,  "service.quartermaster.key.admin"),
-    DbSettings(config, "service.quartermaster.key.common.db")
+    DatabaseConfig(config, "service.quartermaster.key.common.db")
   )
 }

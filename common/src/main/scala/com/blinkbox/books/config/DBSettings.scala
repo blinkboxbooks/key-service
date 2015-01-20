1163779dfbf286deee1192bc51da27a6cf053cc4
JP Hastings-Spital <jphastings@blinkbox.com>
2015-01-20 09:46:56 +0000

    Casing pendantry ;P

--- common/src/main/scala/com/blinkbox/books/config/DBSettings.scala
+++ common/src/main/scala/com/blinkbox/books/config/DBSettings.scala
@@ -3,10 +3,10 @@ package com.blinkbox.books.config
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigException.BadValue
 
-case class DBSettings(url: String, user: String, pass: String)
+case class DbSettings(url: String, user: String, pass: String)
 
-object DBSettings {
-  def apply(config: Config, prefix: String): DBSettings = {
+object DbSettings {
+  def apply(config: Config, prefix: String): DbSettings = {
     val url = config.getUri(s"$prefix.url")
     val port = if (url.getPort != -1) url.getPort else 3306
     val Array(user, pass) = if (url.getUserInfo != null) {
@@ -15,7 +15,7 @@ object DBSettings {
         case _ => throw new BadValue(config.origin(), s"$prefix.url", "Username and password information is missing.")
       }
     } else throw new BadValue(config.origin(), s"$prefix.url", "Username and password information is missing.")
-    DBSettings(
+    DbSettings(
       s"jdbc:${url.getScheme}://${url.getHost}:$port${url.getPath}",
       user,
       pass
